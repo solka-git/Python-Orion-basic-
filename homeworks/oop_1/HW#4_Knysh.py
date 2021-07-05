132c1ece43ff7995b183407c6e6da6cd28a4dbab
Olha Knysh <oliaknish17@gmail.com>
2021-07-06 00:39:56 +0300

    Modified HW#4_Knysh.py

--- homeworks/oop_1/HW#4_Knysh.py
+++ homeworks/oop_1/HW#4_Knysh.py
@@ -17,13 +17,87 @@ class Bus(Venicle):
         print('seating_capacity method')
 
 # 3. Determine which class a given Bus object belongs to (Check type of an object)
+
+print(type(Bus))
+#<class 'type'>
+
+Bus_obj = Bus(100, 100)
+print(type(Bus_obj))
+#<class '__main__.Bus'>
+
 # 4. Determine if School_bus is also an instance of the Vehicle class
+
+School_bus = Bus(140, 1000)
+print(isinstance(School_bus, Venicle))
+print(issubclass(Bus, Venicle))
+
+#True
+#True
+
 # 5. Create a new class School with get_school_id and number_of_students instance attributes
+
+class School:
+    def __init__(self, get_school_id, number_of_students):
+        self.get_school_id = get_school_id
+        self.number_of_students = number_of_students
+
 # 6*. Create a new class SchoolBus that will inherit all of the methods from School and Bus
 # and will have its own - bus_school_color
+
+class SchoolBus(School, Bus):
+    def __init__(self, get_school_id, number_of_students, max_speed, mileage, bus_school_color):
+        super(School).__init__(get_school_id, number_of_students)
+        Bus.__init__(self, max_speed, mileage)
+        self.bus_school_color = bus_school_color
+
+print(SchoolBus.mro())
+
+
 # 7. Polymorphism: Create two classes: Bear, Wolf. Both of them should have make_sound method.
+
+class Bear:
+    def __init__(self, name):
+        self.name = name
+
+    def make_sound(self, sound=''):
+        print(f"{self.name} sound is ")
+        if sound == '':
+            print('Roaar')
+        else:
+            print(sound)
+
+class Wolf:
+    def __init__(self, name):
+        self.name = name
+
+
+    def make_sound(self, sound=''):
+        print(f"{self.name} sound is ")
+        if sound == '':
+            print('Owooo')
+        else:
+            print(sound)
+
+
 # Create two instances, one of Bear and one of Wolf,
 # make a tuple of it and by using for call their action using the same method.
+
+winnie_pooh = Bear('Winnie')
+grey_wolf = Wolf('Grey')
+
+for animal in (winnie_pooh, grey_wolf):
+    animal.make_sound()
+    animal.make_sound('Aaaa')
+
+# Winnie sound is
+# Roaar
+# Winnie sound is
+# Aaaa
+# Grey sound is
+# Owooo
+# Grey sound is
+# Aaaa
+
 # Magic methods:
 # 8. Create class City with name, population instance attributes, return a new instance only when population > 1500,
 # otherwise return message: "Your city is too small".
