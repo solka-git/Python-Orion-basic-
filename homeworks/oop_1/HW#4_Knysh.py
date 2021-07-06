39bbc5752b2cf8f3ceb8cc444fe912a89cacb048
Olha Knysh <oliaknish17@gmail.com>
2021-07-06 21:57:33 +0300

    Modified HW#4_Knysh.py

--- homeworks/oop_1/HW#4_Knysh.py
+++ homeworks/oop_1/HW#4_Knysh.py
@@ -45,12 +45,14 @@ class School:
 # and will have its own - bus_school_color
 
 class SchoolBus(School, Bus):
-    def __init__(self, get_school_id, number_of_students, max_speed, mileage, bus_school_color):
-        super(School).__init__(get_school_id, number_of_students)
-        Bus.__init__(self, max_speed, mileage)
-        self.bus_school_color = bus_school_color
+    pass
 
-print(SchoolBus.mro())
+    def bus_school_color(self):
+        print('bus_school_color method', self.get_school_id)
+
+Sch_bus = SchoolBus(12, 400)
+Sch_bus.seating_capacity()
+Sch_bus.bus_school_color()
 
 
 # 7. Polymorphism: Create two classes: Bear, Wolf. Both of them should have make_sound method.
@@ -101,13 +103,83 @@ for animal in (winnie_pooh, grey_wolf):
 # Magic methods:
 # 8. Create class City with name, population instance attributes, return a new instance only when population > 1500,
 # otherwise return message: "Your city is too small".
+#
+
+class City():
+    def __new__(cls, name, population):
+        if population > 1500:
+            return super().__new__(cls)
+        else:
+            return print("Your city is too small")
+
+    def __init__(self, name, population):
+        self.name = name
+        self.population = population
+
+
+A_ins = City('Kovel', 600)
+#Your city is too small
+
 # 9. Override a printable string representation of the City class and return:
 # The population of the city {name} is {population}
+print('gggggggggggg')
+
+class City:
+    def __new__(cls, name, population):
+        if population > 1500:
+            return super().__new__(cls)
+        else:
+            return print("Your city is too small")
+
+    def __init__(self, name, population):
+        self.name = name
+        self.population = population
+
+    def __str__(self):
+        return f"The population of the city {self.name} is {self.population}"
+
+City1 = City('Kovel', 10560)
+print(City1)
+
+#The population of the city Kovel is 10560
+
 # 10*. Override magic method __add__() to perform the additional action as 'multiply' (*) the value
 # which is greater than 10. And perform this add (+) of two instances.
+
+class Add:
+    def __init__(self, number):
+        self.number = number
+
+    def __add__(self, other):
+       if self.number > 10 or other.number > 10:
+            return self.number * other.number
+       else:
+            return self.number + other.number
+
+a = Add(45)
+b = Add(2)
+print(f"result = {a+b}")
+#result = 90
+a = Add(5)
+b = Add(2)
+print(f"result = {a+b}")
+#result = 7
+
+
 # 11. The __call__ method enables Python programmers to write classes where the instances behave like functions
 # and can be called like a function.
 # Create a new class with __call__ method and define this call to return sum.
+
+class CallSumm:
+    def __call__(self, a, b):
+        return a + b
+
+
+call1 = CallSumm()
+print("Result = ", call1(12, 13))
+
+#Result =  25
+
 # 12*. Making Your Objects Truthy or Falsey Using __bool__().
 # Create class MyOrder with cart and customer instance attributes.
 # Override the __bool__magic method considered to be truthy if the length of the cart list is non-zero.
@@ -118,3 +190,19 @@ for animal in (winnie_pooh, grey_wolf):
 # True
 # bool(order_2)
 # False
+
+
+class MyOrder:
+    def __init__(self, card, customer):
+        self.card = card
+        self.customer = customer
+
+    def __bool__(self):
+        return len(self.card) > 0
+
+order_1 = MyOrder(['a', 'b', 'c'], 'd')
+order_2 = MyOrder([], 'a')
+print(bool(order_1))
+#True
+print(bool(order_2))
+#False
