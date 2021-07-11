831eea5725af26ecf8c752dea0cfea99c1668377
Olha Knysh <oliaknish17@gmail.com>
2021-07-11 13:24:39 +0300

    Modified HW#5_Knysh.py

--- homeworks/oop_2/HW#5_Knysh.py
+++ homeworks/oop_2/HW#5_Knysh.py
@@ -80,9 +80,11 @@ print(pasta_3.ingredients)
 
 # 5*.
 """
+class Concert:
   Make class, which has max_visitors_num attribute and its instances will have visitors_count attribute.
   In case of setting visitors_count - max_visitors_num should be checked,
   if visitors_count value is bigger than max_visitors_num - visitors_count should be assigned with max_visitors_num.
+  
   Example:
       Concert.max_visitor_num = 50
       concert = Concert()
@@ -91,42 +93,86 @@ print(pasta_3.ingredients)
   """
 
 class Concert:
-    @classmethod
-    def max_visitor_num(cls, num):
-        return cls(num)
+    max_visitors_num = 0
 
-    @classmethod
-    def visitors_count(cls, count):
-        if count < Concert.max_visitor_num:
-            print(count, Concert.max_visitor_num)
-            return cls(count)
-        else:
-            print(count, Concert.max_visitor_num)
-            return cls(Concert.max_visitor_num)
+    def __init__(self):
+        self.__visitors_count = 0
 
+    @property
+    def visitors_count(self):
+        return self.__visitors_count
+
+    @visitors_count.setter
+    def visitors_count(self, value):
+        self.__visitors_count = Concert.max_visitor_num if value > Concert.max_visitors_num else value
 
 
 Concert.max_visitor_num = 50
-Con = Concert.max_visitor_num
-print(Con)
 concert = Concert()
 concert.visitors_count = 1000
-print(concert.visitors_count)  # 50
+print(concert.visitors_count)
 
 
 #6.
+"""
+   Create dataclass with 7 fields - key (int), name (str), phone_number (str), address (str), email (str), birthday (str), age (int)
+   """
+
+import dataclasses
+
+@dataclasses.dataclass
 class AddressBookDataClass:
-    """
-    Create dataclass with 7 fields - key (int), name (str), phone_number (str), address (str), email (str), birthday (str), age (int)
-    """
+    key: int
+    name: str
+    phone_number: str
+    address: str
+    email: str
+    birthday: str
+    age: int
+
 
 # 7. Create the same class (6) but using NamedTuple
+
+from collections import namedtuple
+
+AddressBook = namedtuple('Person', ['key', 'name', 'phone_number', 'address', 'email', 'birthday', 'age'])
+
+person_1 = AddressBook(1, 'Solka', '0934567233', 'nizhunska 14', 'solka@mail.com', '17.07', 21)
+
+print(person_1[0])
+print(person_1.name)
+
 # 8.
-class AddressBook:
-    """
-    Create regular class taking 7 params on init - key, name, phone_number, address, email, birthday, age
-    Make its str() representation the same as for AddressBookDataClass defined above.
-    """
+"""
+   Create regular class taking 7 params on init - key, name, phone_number, address, email, birthday, age
+   Make its str() representation the same as for AddressBookDataClass defined above.
+   """
+person = AddressBookDataClass(1, 'Solka', '0934567233', 'nizhunska 14', 'solka@mail.com', '17.07', 21)
+print(person)
+
+# AddressBookDataClass(key=1, name='Solka', phone_number='0934567233', address='nizhunska 14', email='solka@mail.com', birthday='17.07', age=21)
+
+class AddressBookClass:
+    def __init__(self, key, name, phone_number, address, email, birthday, age):
+        self.key = key
+        self.name = name
+        self.phone_number = phone_number
+        self.address = address
+        self.email = email
+        self.birthday = birthday
+        self.age = age
+
+    def __str__(self):
+        return f'AddressBookClass(key={self.key}, name=\'{self.name}\', phone_number=\'{self.phone_number}\', ' \
+               f'address=\'{self.address}\', email=\'{self.email}\', birthday=\'{self.birthday}\', age={self.age})'
+
+person_2 = AddressBookClass(14, 'Olia', '0934567233', 'nizhunska 14', 'solka@mail.com', '17.07', 21)
+print(person_2)
+
+# AddressBookClass(key=14, name='Olia', phone_number='0934567233', address='nizhunska 14', email='solka@mail.com', birthday='17.07', age=21)
+
+
+
 # 9.
 class Person:
     """
@@ -135,6 +181,14 @@ class Person:
     name = "John"
     age = 36
     country = "USA"
+
+person_3 = Person()
+person_3.age = 35
+print(person_3.age)
+#35
+setattr(person_3, 'age', 37)
+print(getattr(person_3, 'age'))
+#37
 
 # 10.
 class Student:
@@ -149,17 +203,38 @@ class Student:
         self.id = id
         self.name = name
 
+
+student_1 = Student(98, 'Solka')
+setattr(student_1, 'email', 'solka@mail.com')
+student_email = getattr(student_1, 'email')
+print(f"Attribute student_email: {student_email}")
+#Attribute student_email: solka@mail.com
+
+
 # 11*.
 class Celsius:
     """
     By using @property convert the celsius to fahrenheit
     Hint: (temperature * 1.8) + 32)
     """
-    def __init__(self, temperature=0):
-        self._temperature = temperature
-
-
 # create an object
 # {obj} = ...
 #
-# print({obj}.temperature)v
+# print({obj}.temperature)
+
+class Celsius:
+    """
+    By using @property convert the celsius to fahrenheit
+    Hint: (temperature * 1.8) + 32)
+    """
+
+    def __init__(self, temperature=0):
+        self._temperature = temperature
+
+    @property
+    def temperature(self):
+        return (self._temperature * 1.8) + 32
+
+temp_1 = Celsius(30)
+print(temp_1.temperature)
+# 86.0
